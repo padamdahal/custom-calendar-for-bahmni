1f99f0edf9735987be35f8c26de62dcec188f7f0
padamdahal <dahal0514@gmail.com>
2019-03-19 14:56:31 +0545

    Update customCalendar.js

--- customCalendar.js
+++ customCalendar.js
@@ -1,6 +1,3 @@
-// Author: Padam Dahal, Technical Advisor, GIZ/S2HSP (Nepal)
-// This nepali calendar customization is based on the BAHMNI 0.90 version
-// Modification might require for other versions of BAHMNI
 
 // Nepali datepicker everywhere
 try{
@@ -13,60 +10,36 @@ try{
 				customDateField.appendTo(html);
 				
 				// if value is set translate to nepali and fill the date
+				// Get the model that holds the data value
 				var model = element.attr('ng-model');
-				var adDate;
-				if(model == 'patient.birthdate'){
-					if($scope.patient.birthdate != null || $scope.patient.birthdate != ""){
-						adDate = new Date($scope.patient.birthdate);
-						
-						// Modify for other calendars based on the calendar implementation /////////////////////////////////////////////////////////
-						var customDate = calendarFunctions.getBsDateByAdDate(adDate.getFullYear(), adDate.getMonth()+1, adDate.getDate());
-						customDate = calendarFunctions.bsDateFormat("%y-%m-%d", customDate.bsYear, customDate.bsMonth, customDate.bsDate);
-						////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-						
-						customDateField.val(customDate);
+				
+				try{
+					if(eval('$scope.'+model) != null || eval('$scope.'+model) != 'undefined' && eval('$scope.'+model).getfullYear() != 1970){		
+						var adDate = new Date(eval('$scope.'+model));
+
+						var converter = new DateConverter();
+						converter.setEnglishDate(adDate.getFullYear(), adDate.getMonth()+1, adDate.getDate());						
+						customDateField.val(converter.toNepaliString());
 					}
-				}else if(model == 'newRelationship.endDate'){
-					adDate = $scope.newRelationship.endDate;
-				}else if(model == 'patient.deathDate'){
-					adDate = $scope.patient.deathDate;
-				}else if(model == 'programEnrollmentDate' || model == 'targetModel[attribute.name]'){
-					// for programs
-				}else if(model == 'date'){
-					// for retrospective data entry
-				}else{
-					// For Observation of date type
-					if($scope.hasOwnProperty('observation')){
-						if($scope.observation.value != null && $scope.observation.value != "" && $scope.observation.value !== undefined){
-							adDate = new Date($scope.observation.value);
-							
-							// Modify for other calendars based on the calendar implementation ///////////////////////////////////////////////////////// 
-							var customDate = calendarFunctions.getBsDateByAdDate(adDate.getFullYear(),adDate.getMonth()+1, adDate.getDate());
-							customDate = calendarFunctions.bsDateFormat("%y-%m-%d", customDate.bsYear, customDate.bsMonth, customDate.bsDate)
-							////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-							
-							customDateField.val(customDate);
-						}
-					}
+				}catch(e){
+					console.log('Error converting to the custom date.');
 				}
 				
-				// Assign custom datepicker in the new input field
-				// Modify for other calendars based on calendar implementation ///////////
-				customDateField.nepaliDatePicker({
-					dateFormat: "%y-%m-%d",
-					closeOnDateSelect: true
+				customDateField.calendarsPicker({
+					calendar: $.calendars.instance('nepali'),
+					yearRange: '-120:+30',
+					duration: "fast",
+					showAnim: "",
+					dateFormat: 'yyyy-mm-dd',
+					onSelect: function(npDate) {
+						// Conversion and assignment
+						var converter = new DateConverter();
+						converter.setNepaliDate(npDate[0]._year, npDate[0]._month, npDate[0]._day)
+						customDateField.prev('input').val(converter.toEnglishString());
+						customDateField.prev('input').trigger("change");
+						customDateField.prev('input').trigger("blur");
+					}
 				});
-				/////////////////////////////////////////////////////////////////////////
-				
-				// Update the date in ad Date on nepali date select
-				// Modify for other calendars based on calendar implementation //////////////////
-				customDateField.on("dateSelect", function (event) {
-					var adDate = calendarFunctions.formattedAd(event.datePickerData.adDate);
-					customDateField.prev('input').val(adDate);
-					customDateField.prev('input').trigger("change");
-					customDateField.prev('input').trigger("blur");
-				});
-				//////////////////////////////////////////////////////////////////////////////////
 			}
 		};
 		return { link: link };
@@ -78,34 +51,38 @@ try{
 // Clinical dashboard (display)
 try {
 	var clinicalApp = angular.module('bahmni.clinical');
-
+	
 	// List of visits are wrapped in a tag with class name 'visit'
 	clinicalApp.directive('a', function () {
 		var link = function ($scope, element, attrs, ngModel) {
 			// Patient dashboard, visit list
+			
 			if(element.context.className === 'visit'){
+				
+				var model = element.attr('ng-model');
 				// for visit start date
 				var startDate = new Date($scope.visit.startDatetime);
 				
-				// Modify for other calendars based on the calendar implementation /////////////////////////////////////////
-				var startDateCustom = calendarFunctions.getBsDateByAdDate(startDate.getFullYear(),startDate.getMonth()+1,startDate.getDate());
-				startDateCustom = calendarFunctions.bsDateFormat('%y|%m|%d', startDateCustom.bsYear, startDateCustom.bsMonth, startDateCustom.bsDate);
-				//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				// Date conversion
+				var converter = new DateConverter();
+				converter.setEnglishDate(startDate.getFullYear(), startDate.getMonth()+1, startDate.getDate());		
+				var customDate = converter.toNepaliStringLong();
+				// End date conversion
 				
-				$scope.visit.startDatetime = startDateCustom
+				$scope.visit.startDatetime = customDate;
 				
 				// for visit end date
 				if($scope.visit.hasOwnProperty('stopDatetime') && $scope.visit.stopDatetime != null){
 					var endDate = new Date($scope.visit.stopDatetime);
+					// Date conversion
+					var converter = new DateConverter();
+					converter.setEnglishDate(endDate.getFullYear(), endDate.getMonth()+1, endDate.getDate());
+					var customDate = converter.toNepaliStringLong();
+					// End date conversion
 					
-					// Modify for other calendars based on the calendar implementation /////////////////////////////////////////////////
-					var endDateCustom = calendarFunctions.getBsDateByAdDate(endDate.getFullYear(), endDate.getMonth()+1, endDate.getDate());
-					endDateCustom = calendarFunctions.bsDateFormat('%y|%m|%d', endDateCustom.bsYear, endDateCustom.bsMonth, endDateCustom.bsDate)
-					/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-					
-					$scope.visit.stopDatetime = endDateCustom;
+					$scope.visit.stopDatetime = customDate;
 				}
-			}	
+			}			
 		};
 		return { link: link };
 	});
@@ -118,10 +95,11 @@ try {
 			if(element.context.className === 'obs-date'){
 				var encDate = new Date(Date($scope.obsGroup.date));
 				
-				// Modify for other calendars based on calendar implementation //////////////////////////////////////////////////////////////////////////////////////////////////////
-				var customDate = calendarFunctions.getBsDateByAdDate(encDate.getFullYear(),encDate.getMonth()+1,encDate.getDate());
-				customDate = calendarFunctions.bsDateFormat('%y|%m|%d', customDate.bsYear, customDate.bsMonth, customDate.bsDate)+" "+encDate.getHours()+":"+encDate.getMinutes();
-				/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				// Date conversion
+				var converter = new DateConverter();
+				converter.setEnglishDate(encDate.getFullYear(), encDate.getMonth()+1, encDate.getDate());						
+				var customDate = converter.toNepaliStringLong();
+				// End date conversion
 				
 				$scope.obsGroup.date = customDate;
 			}
@@ -131,12 +109,14 @@ try {
 			if(element.attr('id') === 'diagnosisDate'){
 				var diagDate = new Date($scope.diagnosis.diagnosisDateTime);
 				
-				// Modify for other calendars based on calendar implementation //////////////////////////////////////////////////////////////////////////////////////////////////////
-				var customDate = calendarFunctions.getBsDateByAdDate(diagDate.getFullYear(),diagDate.getMonth()+1,diagDate.getDate());
-				customDate = calendarFunctions.bsDateFormat('%y|%m|%d', customDate.bsYear, customDate.bsMonth, customDate.bsDate)+" "+diagDate.getHours()+":"+diagDate.getMinutes();
-				/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				// Date Conversion
+				var converter = new DateConverter();
+				converter.setEnglishDate(diagDate.getFullYear(), diagDate.getMonth()+1, diagDate.getDate());						
+				var customDate = converter.toNepaliStringLong();
+				// End date conversion
 				
 				$scope.diagnosis.diagnosisDateTime = customDate;
+				
 				if($scope.diagnosis.hasOwnProperty('latestDiagnosis') && $scope.diagnosis.latestDiagnosis != null){
 					$scope.diagnosis.latestDiagnosis.diagnosisDateTime = customDate;
 				}
@@ -150,25 +130,28 @@ try {
 				// for visit start date				
 				var startDate = new Date($scope.visitSummary.startDateTime);
 				
-				// Modify for other calendars based on calendar implementation //////////////////////////////////////////////////////////////////////////////////
-				var startDateCustom = calendarFunctions.getBsDateByAdDate(startDate.getFullYear(), startDate.getMonth()+1, startDate.getDate());
-				startDateCustom_formatted = calendarFunctions.bsDateFormat('%y|%m|%d', startDateCustom.bsYear, startDateCustom.bsMonth, startDateCustom.bsDate);
-				/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+				// Date Conversion
+				var converter = new DateConverter();
+				converter.setEnglishDate(startDate.getFullYear(), startDate.getMonth()+1, startDate.getDate());						
+				var customDate = converter.toNepaliStringLong();
+				// End date conversion
+				$scope.visitSummary.startDateTime = customDate;
+				//element.context.children[0].innerHTML = startDateCustom_formatted;
+				
 				// for visit stop date
 				if($scope.visitSummary.hasOwnProperty('stopDateTime') && $scope.visitSummary.stopDateTime != null){
 					var endDate = new Date($scope.visitSummary.stopDateTime);
 					
-					// Modify for other calendars based on calendar implementation /////////////////////////////////////////////////////////////////////////////////////
-					var stopDateCustom = calendarFunctions.getBsDateByAdDate(endDate.getFullYear(), endDate.getMonth()+1, endDate.getDate());
-					stopDateCustom_formatted = '- '+calendarFunctions.bsDateFormat('%y|%m|%d', stopDateCustom.bsYear, stopDateCustom.bsMonth, stopDateCustom.bsDate);
-					/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+					// Date Conversion
+					var converter = new DateConverter();
+					converter.setEnglishDate(endDate.getFullYear(), endDate.getMonth()+1, endDate.getDate());						
+					var customDate = converter.toNepaliStringLong();
+					// End date conversion
+					$scope.visitSummary.stopDateTime = customDate
+					
+					//element.context.children[1].innerHTML = stopDateCustom_formatted;
 
-				}
-
-				element.context.children[0].innerHTML = startDateCustom_formatted;
-				element.context.children[1].innerHTML = stopDateCustom_formatted;
-				
+				}				
 			}
 		};
 		return { link: link };
@@ -183,13 +166,13 @@ try {
 					if($scope.observation.concept.dataType == 'Date'){
 						var obsValueDate = new Date($scope.observation.value);
 						
-						// Modify for other calendars based on calendar implementation ////////////////////////////////////////////////////////////
-						var obsValueDateCustom = calendarFunctions.getBsDateByAdDate(obsValueDate.getFullYear(), obsValueDate.getMonth()+1, obsValueDate.getDate());
-						//obsValueDateCustom = obsValueDateCustom.bsYear+'|'+obsValueDateCustom.bsMonth+'|'+obsValueDateCustom.bsDate;
-						obsValueDateCustom = calendarFunctions.bsDateFormat('%y|%m|%d',obsValueDateCustom.bsYear, obsValueDateCustom.bsMonth, obsValueDateCustom.bsDate);
-						///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+						// Date Conversion
+						var converter = new DateConverter();
+						converter.setEnglishDate(obsValueDate.getFullYear(), obsValueDate.getMonth()+1, obsValueDate.getDate());						
+						var customDate = converter.toNepaliStringLong();
+						// End date conversion
 						
-						$scope.observation.value = obsValueDateCustom;
+						$scope.observation.value = customDate;
 					}
 				}
 			}	
@@ -204,15 +187,15 @@ try {
 			if(element.context.className === 'compuptedValue'){
 				// Todo : check for the valid datetime value
 				
-				
 				var obsValueDateComputed = new Date($scope.observation.value);
 				
-				// Modify for other calendars based on calendar implementation /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-				var obsValueDateComputedCustom = calendarFunctions.getBsDateByAdDate(obsValueDateComputed.getFullYear(), obsValueDateComputed.getMonth()+1, obsValueDateComputed.getDate());
-				obsValueDateComputedCustom = calendarFunctions.bsDateFormat('%y|%m|%d',obsValueDateComputedCustom.bsYear, obsValueDateComputedCustom.bsMonth, obsValueDateComputedCustom.bsDate);
-				////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				// Date Conversion
+				var converter = new DateConverter();
+				converter.setEnglishDate(obsValueDateComputed.getFullYear(), obsValueDateComputed.getMonth()+1, obsValueDateComputed.getDate());						
+				var customDate = converter.toNepaliStringLong();
+				// End date conversion
 				
-				$scope.observation.value = obsValueDateComputedCustom;
+				$scope.observation.value = customDate;
 			}			
 		};
 		return { link: link };
